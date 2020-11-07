48042e48495eaad7e284a936c2dbe9a764589a8c
surendranunna <surendranunna98@gmail.com>
2020-11-07 09:23:33 +0530

    4th commit

--- UniversityApp/Form1.cs
+++ UniversityApp/Form1.cs
@@ -25,11 +25,11 @@ namespace UniversityApp
             FillCoursesTable();
             SelectedCoursesTable();
 
-            listBox1.DataSource = dtCourses;
-            listBox1.DisplayMember = "CourseName";
+            ListBox1.DataSource = dtCourses;
+            ListBox1.DisplayMember = "CourseName";
              
-            listBox2.DataSource = dtSelectedCourses;
-            listBox2.DisplayMember = "CourseName";
+            ListBox2.DataSource = dtSelectedCourses;
+            ListBox2.DisplayMember = "CourseName";
 
 
         }
@@ -54,63 +54,63 @@ namespace UniversityApp
             dtSelectedCourses.Columns.Add("CourseDuration");
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
+        private void BtnAdd_Click(object sender, EventArgs e)
         {
-            if (listBox1.Items.Count > 0)
+            if(ListBox1.Items.Count > 0)
             {
-                dtSelectedCourses.ImportRow(dtCourses.Rows[listBox1.SelectedIndex]);
-                dtCourses.Rows[listBox1.SelectedIndex].Delete();
+                dtSelectedCourses.ImportRow(dtCourses.Rows[ListBox1.SelectedIndex]);
+                dtCourses.Rows[ListBox1.SelectedIndex].Delete();
             }
         }
 
-        private void btnRemove_Click(object sender, EventArgs e)
+        private void BtnRemove_Click(object sender, EventArgs e)
         {
-            if (listBox2.Items.Count > 0)
+            if(ListBox2.Items.Count > 0)
             {
-                dtCourses.ImportRow(dtSelectedCourses.Rows[listBox2.SelectedIndex]);
-                dtSelectedCourses.Rows[listBox2.SelectedIndex].Delete();
+                dtCourses.ImportRow(dtSelectedCourses.Rows[ListBox2.SelectedIndex]);
+                dtSelectedCourses.Rows[ListBox2.SelectedIndex].Delete();
             }
         }
 
-        private void btnAddAll_Click(object sender, EventArgs e)
+        private void BtnAddAll_Click(object sender, EventArgs e)
         {
-            if (listBox1.Items.Count > 0)
+            if(ListBox1.Items.Count > 0)
             {
                 int count = dtCourses.Rows.Count;
-                for (int i = count - 1; i >= 0; i--)
+                for(int i = count-1; i >= 0; i--)
                 {
-                    dtSelectedCourses.ImportRow(dtCourses.Rows[listBox1.SelectedIndex]);
-                    dtCourses.Rows[listBox1.SelectedIndex].Delete();
+                    dtSelectedCourses.ImportRow(dtCourses.Rows[ListBox1.SelectedIndex]);
+                    dtCourses.Rows[ListBox1.SelectedIndex].Delete();
                 }
             }
         }
 
-        private void btnRemoveAll_Click(object sender, EventArgs e)
+        private void BtnRemoveAll_Click(object sender, EventArgs e)
         {
 
-            if (listBox2.Items.Count > 0)
+            if(ListBox2.Items.Count > 0)
             {
                 int count = dtSelectedCourses.Rows.Count;
-                for (int i = count - 1; i >= 0; i--)
+                for(int i = count-1; i >= 0; i--)
                 {
-                    dtCourses.ImportRow(dtSelectedCourses.Rows[listBox2.SelectedIndex]);
-                    dtSelectedCourses.Rows[listBox2.SelectedIndex].Delete();
+                    dtCourses.ImportRow(dtSelectedCourses.Rows[ListBox2.SelectedIndex]);
+                    dtSelectedCourses.Rows[ListBox2.SelectedIndex].Delete();
                 }
             }
         }
 
-        private void btnFinilize_Click(object sender, EventArgs e)
+        private void BtnFinilize_Click(object sender, EventArgs e)
         {
             DialogResult dr = MessageBox.Show("Are you sure you want to finilize the courses", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
               if(dr==DialogResult.Yes)
             {
-                dataGridView1.DataSource = dtSelectedCourses;
-                dataGridView1.Enabled = false;
-                dataGridView1.Columns[0].Visible = false;
-                dataGridView1.RowHeadersVisible = false;
+                DataGridView1.DataSource = dtSelectedCourses;
+                DataGridView1.Enabled = false;
+                DataGridView1.Columns[0].Visible = false;
+                DataGridView1.RowHeadersVisible = false;
 
-                dataGridView1.Columns[1].Width = 350;
-                dataGridView1.Columns[2].Width = 500;
+                DataGridView1.Columns[1].Width = 350;
+                DataGridView1.Columns[2].Width = 500;
 
 
             }
